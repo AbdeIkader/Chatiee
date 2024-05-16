cf76b3833b63fbbfafc8bca34e1af432b2351329
Shehab Eldin Said <Shehab.eldin.said1@gmail.com>
2024-05-16 22:56:04 +0300

    login form UI

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
+import Login from './pages/Login/Login'
 
-function App() {
+export default function App() {
 
 
   return <>
+    
+    <div className='p-4 h-screen flex items-center justify-center'>
+          <Login/>
+    </div>
   
-    </>
+  </>
   
+
+
 }
 
-export default App
+
