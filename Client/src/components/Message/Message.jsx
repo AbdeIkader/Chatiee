04426989081f9f2b8e4e1ae9946a5a95c8172aad
Shehab Eldin Said <Shehab.eldin.said1@gmail.com>
2024-05-18 17:58:46 +0300

    handle signup form

--- Client/src/components/Message/Message.jsx
+++ Client/src/components/Message/Message.jsx
@@ -8,7 +8,7 @@ export default function Message() {
                 <img src="https://avatars.githubusercontent.com/u/119017520?v=4" />
                 </div>
         </div>
-        <div className={`chat-bubble text-white bg-gray-200 text-black`}> بقولك اغال انا مسافر دبي بكرة و مراتي جاية معايا عشان حنقابل نجيب ساويرس جاية منك تيجي انت والمدام </div>
+        <div className={`chat-bubble text-white bg-gray-400 text-black`}> اشطا اغال</div>
         <div className={`chat-footer text-xs opacity-50 flex gap-1 items-center`}>12:15 AM</div>
 
     </div>
--- Client/src/pages/Signup/GenderCheckBox.jsx
+++ Client/src/pages/Signup/GenderCheckBox.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 
-export default function GenderCheckBox() {
-  return <>
+export default function GenderCheckBox({ onCheckBoxChange, selectedGender }) {
+  return (
     <div className="flex justify-start items-center gap-8">
-        
-<div class="flex items-center mb-4">
-    <input id="MaleCheckbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded "/>
-    <label for="MaleCheckbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Male</label>
-</div>
-<div class="flex items-center mb-4">
-    <input id="FemaleCheckbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"/>
-    <label for="FemaleCheckbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Female</label>
-</div>
-
+      <div className="flex items-center mb-4">
+        <input 
+          id="Male"
+          type="checkbox"
+          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
+          checked={selectedGender === "Male"}
+          onChange={() => onCheckBoxChange("Male")}
+        />
+        <label htmlFor="Male" className={`${selectedGender === "Male" ? "selected" : ""} ms-2 text-sm font-medium text-gray-900 dark:text-gray-300`}>
+          Male
+        </label>
+      </div>
+      <div className="flex items-center mb-4">
+        <input 
+          id="Female"
+          type="checkbox"
+          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
+          checked={selectedGender === "Female"}
+          onChange={() => onCheckBoxChange("Female")}
+        />
+        <label htmlFor="Female" className={`${selectedGender === "Female" ? "selected" : ""} ms-2 text-sm font-medium text-gray-900 dark:text-gray-300`}>
+          Female
+        </label>
+      </div>
     </div>
-  </>
+  );
 }
