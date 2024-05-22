c1f51fb6f09eb33083906b580669269768cc273a
Shehab Eldin Said <Shehab.eldin.said1@gmail.com>
2024-05-22 22:09:18 +0300

    update users list - Logout btn - search bar - sidebar
    Co-authored-by: Abdelrahman Mohamed Abdelkader <119017520+AbdeIkader@users.noreply.github.com>
    Co-authored-by: Shehab Eldin Said <92757027+Shehab1001@users.noreply.github.com>

--- Client/src/components/Sidebar/LogoutButton.jsx
+++ Client/src/components/Sidebar/LogoutButton.jsx
@@ -1,20 +1,17 @@
-import { BiLogOut } from 'react-icons/bi';
+import { BiLogOut } from "react-icons/bi";
 import useLogout from "../../hooks/useLogout";
 
+const LogoutButton = () => {
+	const { loading, logout } = useLogout();
 
-export default function LogoutButton() {
-
-  const { loading, logout } = useLogout();
-
-  return <>
-
-    <div className='bg-red-100 py-2 rounded-t-xl '>
-      {!loading ? (
-        <BiLogOut className='w-6 h-6 text-red-600 cursor-pointer' onClick={logout} />
-      ) : (
-        <span className='loading loading-spinner'></span>
-      )}
-    </div>
-
-  </>
-}
+	return (
+		<div className='mt-auto'>
+			{!loading ? (
+				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+			) : (
+				<span className='loading loading-spinner'></span>
+			)}
+		</div>
+	);
+};
+export default LogoutButton;
--- Client/src/components/Sidebar/SearchInput.jsx
+++ Client/src/components/Sidebar/SearchInput.jsx
@@ -1,45 +1,56 @@
 import { useState } from "react";
-import { FiSearch } from 'react-icons/fi';
+import { IoSearchSharp } from "react-icons/io5";
 import useConversation from "../../zustand/useConversation";
 import useGetConversations from "../../hooks/useGetConversations";
 import toast from "react-hot-toast";
 
-export default function SearchInput() {
+const SearchInput = () => {
+	const [search, setSearch] = useState("");
+	const { setSelectedConversation } = useConversation();
+	const { conversations } = useGetConversations();
 
-  const [search, setSearch] = useState("");
-  const { setSelectedConversation } = useConversation();
-  const { conversations } = useGetConversations();
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!search) return;
+		if (search.length < 3) {
+			return toast.error("Search term must be at least 3 characters long");
+		}
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!search) return;
-    if (search.length < 3) {
-      return toast.error("Search term must be at least 3 characters long");
-    }
+		const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
 
-    const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
+		if (conversation) {
+			setSelectedConversation(conversation);
+			setSearch("");
+		} else toast.error("No such user found!");
+	};
+	return (
+		<form onSubmit={handleSubmit} className='flex items-center gap-2'>
+			<input
+				type='text'
+				placeholder='Search…'
+				className='input input-bordered rounded-full'
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+			<button type='submit' className='btn btn-circle bg-sky-500 text-white'>
+				<IoSearchSharp className='w-6 h-6 outline-none' />
+			</button>
+		</form>
+	);
+};
+export default SearchInput;
 
-    if (conversation) {
-      setSelectedConversation(conversation);
-      setSearch("");
-    } else toast.error("No such user found!");
-  };
+// STARTER CODE SNIPPET
+// import { IoSearchSharp } from "react-icons/io5";
 
-  return (
-    <form onSubmit={handleSubmit} >
-      <div className="h-[40px] flex items-center bg-white border rounded-md p-2 shadow-sm m-2">
-        <button type='submit'>
-          <FiSearch className="text-gray-500 ml-1" size={20} />
-        </button>
-        <input
-          type="text"
-          placeholder="Search..."
-          className="ml-2 w-full outline-none"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </div>
-    </form>
-
-  );
-}
+// const SearchInput = () => {
+// 	return (
+// 		<form className='flex items-center gap-2'>
+// 			<input type='text' placeholder='Search…' className='input input-bordered rounded-full' />
+// 			<button type='submit' className='btn btn-circle bg-sky-500 text-white'>
+// 				<IoSearchSharp className='w-6 h-6 outline-none' />
+// 			</button>
+// 		</form>
+// 	);
+// };
+// export default SearchInput;
