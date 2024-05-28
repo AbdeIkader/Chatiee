80d6cb827e8b3a2317a086608d05e67ef210aded
AbdeIkader <abdelrahmanabdelkader2002@gmail.com>
2024-05-28 03:36:25 +0300

    Fixed some bugs

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/SignUp";
+
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import SignUp from './pages/Signup/Signup';
 
 function App() {
 	const { authUser } = useAuthContext();
